05d67511c92523f975e4856d4b58670bc033fd8f
vlymar90 <7ua.lymar2014@gmail.com>
2020-11-27 11:10:57 +0300

    решение дз3 задание 1

--- geekbrains/src/ru/geekbrains/dz/Main.java
+++ geekbrains/src/ru/geekbrains/dz/Main.java
@@ -1,48 +1,51 @@
 package ru.geekbrains.dz;
 
 
-import java.util.regex.Pattern;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        int result = 0;
-        String[][] list = {{"15","2","24","2"},{"24","3","23","7"},{"5","6","8","10"},{"21","20","21","32","2"}};
-        try {
-            result = transformationArrays(list);
-        } catch (MyArraySizeException e) {
-          System.out.println(e.getInfo());
-        }
-        catch (MyArrayDataException ex) {
-            System.out.println("Результат работы программы: " + ex.result);
-            System.out.println(ex.getS());
-            return;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*ArrayList<String> list = new ArrayList<>(Arrays.asList("ошибка", "исключение", "исключение", "ошибка",
+                "массив", "переменная", "мышь", "мышь", "принтер", "свет", "телефон", "работа", "дом", "машина",
+                "машина", "ошибка"));
+        HashSet<String> set = new HashSet<>(list);
+        System.out.println(set);
+        for(String line : set) {
+            System.out.println(line + " встречается: " + countRepeat(line, list) + " раз(а).");
         }
 
-        System.out.println("Результат работы программы: " + result);
     }
-
-    public static int transformationArrays(String[][] list) throws MyArraySizeException, MyArrayDataException {
-        int index = 0;
-        int number = 0;
-        int sum = 0;
-
-        for (String[] upList : list) {
-            if(list.length > 4 || upList.length > 4) {
-                throw new MyArraySizeException("Длина массива должна быть не больше 4 на 4");
-            }
-            for (String line : upList) {
-                     try {
-                         number = Integer.parseInt(line);
-                         sum += number;
-                     }
-                     catch (NumberFormatException e) {
-                         throw new MyArrayDataException("Ошибка в ячейки номер " + index, sum);
-                     }
-                index++;
+    public static int countRepeat(String line, ArrayList<String> list) {
+        int countLine = 0;
+        for(String lineList : list) {
+            if(line.equals(lineList)) {
+                countLine++;
             }
         }
-        return sum;
+        return countLine; */
     }
 }
 
